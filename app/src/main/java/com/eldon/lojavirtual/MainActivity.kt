935e2e4762181eb3d5f58298503af97189ecf64d
eacosta1956 <eldon.costa@terra.com.br>
2026-08-24 01:42:47 -0300

    feat: configurar home com navigation component

--- app/src/main/java/com/eldon/lojavirtual/MainActivity.kt
+++ app/src/main/java/com/eldon/lojavirtual/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.eldon.lojavirtual
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.eldon.lojavirtual.databinding.ActivityMainBinding
 
@@ -12,31 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        configureClickListeners()
-    }
-
-    private fun configureClickListeners() {
-        binding.buttonProducts.setOnClickListener {
-            showMessage("Produtos")
-        }
-
-        binding.buttonCarts.setOnClickListener {
-            showMessage("Carrinho")
-        }
-
-        binding.buttonUsers.setOnClickListener {
-            showMessage("Usuários")
-        }
-
-        binding.buttonPosts.setOnClickListener {
-            showMessage("Postagens")
-        }
-    }
-
-    private fun showMessage(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
